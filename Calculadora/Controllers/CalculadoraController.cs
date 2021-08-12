aa7d62e08ce93235e41c0bed50cc603b6e72f7a4
JackoCol <ccja1985@gmail.com>
2021-08-12 01:24:16 -0500

    ,

---
+++ Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Calculadora.Models;
+
+namespace Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalculadoraController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
